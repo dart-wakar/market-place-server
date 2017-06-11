dc40c73180762d828c925a41d62c31db08454350
dart-wakar <wakar@whitelogic.in>
2017-06-12 02:57:10 +0530

    Allow CORS

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ var ProductModel = require('./models/product');
 var UserModel = require('./models/user');
 
 router.use(function(req,res,next) {
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
     console.log('An api request is made');
     next();
 });
